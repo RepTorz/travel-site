3bee69e99e69e01e6582a4db7edd64463cc5db0a
Tony Wong <twwon17@gmail.com>
2018-10-13 10:18:05 +0000

    SVG sprite added

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    svgSprite = require('gulp-svg-sprite'),
+    rename = require('gulp-rename');
+    
+var config = {
+    mode: {
+        css: {
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }    
+        }
+    }
+}    
+    
+gulp.task('createSprite', function() {
+    // **means all sub folders as well
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySpriteCSS', function() {
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
