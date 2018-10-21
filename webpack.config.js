9c168108822504c713a8c7d5d088097b11025f54
Tony Wong <twwon17@gmail.com>
2018-10-21 00:03:33 +0000

    Completed desktop and mobile header styles

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        // absolute path of our current folder
+        path: path.resolve(__dirname, "./app/temp/scripts"),
+        filename: "App.js"
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            }          
+        ]
+    }
+}
